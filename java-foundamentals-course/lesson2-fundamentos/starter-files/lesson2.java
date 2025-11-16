2256be23c2eae26c55b8e233562199370126f373
MassiFlip <massinv123@gmail.com>
2025-11-15 20:02:57 -0500

    Add starter code for lesson 2
    Initial implementation of lesson2 starter code.

---
+++ java-foundamentals-course/lesson2-fundamentos/starter-files/lesson2.java
@@ -0,0 +1,10 @@
+public class Leccion2Starter {
+
+    public static void main(String[] args) {
+
+        // TODO: Crea variables de tipo int, double, boolean y String
+        // TODO: Muestra sus valores en consola
+        // TODO: Realiza operaciones matemáticas simples
+        // TODO: Realiza comparaciones y muestra los resultados
+    }
+}
